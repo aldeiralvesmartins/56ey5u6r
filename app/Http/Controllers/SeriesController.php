5db22853ba7e0074a657047695f4df5cf81c311f
aldeir <aldeir@r2soft.com.br>
2023-01-11 08:27:55 -0300

    11-01

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,13 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
-use App\Mail\SeriesCreated;
+use App\Jobs\DeleteSeriesCover;
 use App\Models\Series;
-use App\Models\User;
 use App\Repositories\SeriesRepository;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Mail;
 
 class SeriesController extends Controller
 {
@@ -34,9 +31,7 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
-        $coverPath = $request->hasFile('cover')
-            ? $request->file('cover')->store('series_cover', 'public')
-            : null;
+        $coverPath = $request->file('cover')?->store('series_cover', 'public');
         $request->coverPath = $coverPath;
         $serie = $this->repository->add($request);
         \App\Events\SeriesCreated::dispatch(
@@ -53,6 +48,7 @@ class SeriesController extends Controller
     public function destroy(Series $series)
     {
         $series->delete();
+        DeleteSeriesCover::dispatch($series->cover);
 
         return to_route('series.index')
             ->with('mensagem.sucesso', "Série '{$series->nome}' removida com sucesso");
